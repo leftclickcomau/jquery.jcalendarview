29835c19cb1c6bdbfe43cd1ebeed8902e62588a4
Ben New <ben@leftclick.com.au>
2009-07-13 16:36:03 +0000

    ben: jcalendarview: improved OO

--- jcalendarview/jquery.jcalendarview.js
+++ jcalendarview/jquery.jcalendarview.js
@@ -1,81 +1,104 @@
 /*
- * jCalendarView
+ * jcalendarview
  * 
  * Continuous improvement to convert an unordered list into a calendar view.
  * 
  * TODO License
  */
 
-(function($) {
+;(function($) {
 	
 	/**
 	 * Convert a &lt;ul&gt; element into a calendar view.
 	 * 
 	 * TODO Document options
 	 */
-	$.fn.calendarView = function(options) {
-		var options = $.extend({}, $.calendarView.defaultOptions, options);
+	$.fn.jcalendarview = function(options, date) {
 		var calendar = null;
 		$(this).each(function() {
 			if (this.nodeName.toLowerCase() == 'ul') {
-				calendar = $.calendarView._init(this, options);
+				calendar = new $.jcalendarview(this, options, date);
 			}
 		});
 		return calendar;
 	};
 	
 	/**
-	 * Utility functions for calendar view widget.
+	 * Initialise the given element as a calendar view.
+	 * 
+	 * @param elem &lt;ul&gt; element to convert into a calendar view
+	 * @param options See $.fn.jcalendarview(options)
 	 */
-	$.calendarView = {
+	$.jcalendarview = function(elem, options, date) {
+		this.options = $.extend({}, $.jcalendarview.defaultOptions, options);
+		this.date = this.fixDate(date ? date : new Date());
+		this.elem = $(elem);
+		this.container = $('<div class="' + this.options.calendarViewClass + '"></div>');
+		this.elem.parent().append(this.container);
+		this.render();
+		this.container.append('<div style="clear:both;"></div>');
+		this.container.css('display', 'inline-block');
+		this.elem.css('display', 'none');
+		return this.container;
+	};
+
+	/**
+	 * Calendar view default options.
+	 */
+	$.jcalendarview.defaultOptions = {
+		'displayControls' : true,
+		'minDate' : null,
+		'maxDate' : null,
+		
+		'dateFieldInputClass' : 'date',
+		'previewTextInputClass' : 'previewText',
+		'fullTextInputClass' : 'fullText',
+	
+		'calendarViewClass' : 'cvCalendarView',
+		'monthYearClass' : 'cvMonthYear',
+		'previousMonthLinkClass' : 'cvPreviousMonth',
+		'nextMonthLinkClass' : 'cvNextMonth',
+		'dayNameDivClass' : 'cvDayNameDiv',
+		'dayNameLabelClass' : 'cvDayNameLabel',
+		'dayClass' : 'cvDay',
+		'dayOutOfMonthClass' : 'cvDayOutOfMonth',
+		'pastDateClass' : 'cvPastDate',
+		'currentDateClass' : 'cvCurrentDate',
+		'futureDateClass' : 'cvFutureDate',
+		'dayNumberClass' : 'cvDayNumber',
+		'currentDateLabelClass' : 'cvCurrentDateLabel',
+		'innerClass' : 'cvInner',
+		'dateFieldClass' : 'cvDate',
+		'previewTextContainerClass' : 'cvPreviewText',
+		'fullTextContainerClass' : 'cvFullText',
+		
+		'currentDateLabelText' : 'today',
+		'previousMonthLinkText' : 'previous',
+		'nextMonthLinkText' : 'next'
+	};
+	
+	/**
+	 * Instance methods for calendar view widget.
+	 */
+	$.extend($.jcalendarview.prototype, {
 		
 		/**
-		 * Calendar view default options.
-		 */
-		defaultOptions : {
-			'displayControls' : true,
-			'currentDate' : new Date(),
-			'minDate' : null,
-			'maxDate' : null,
-			
-			'dateFieldInputClass' : 'date',
-			'previewTextInputClass' : 'previewText',
-			'fullTextInputClass' : 'fullText',
-		
-			'calendarViewClass' : 'cvCalendarView',
-			'monthYearClass' : 'cvMonthYear',
-			'dayNameDivClass' : 'cvdayNameDiv',
-			'dayNameLabelClass' : 'cvDayNameLabel',
-			'dayClass' : 'cvDay',
-			'dayOutOfMonthClass' : 'cvDayOutOfMonth',
-			'pastDateClass' : 'cvPastDate',
-			'currentDateClass' : 'cvCurrentDate',
-			'futureDateClass' : 'cvFutureDate',
-			'dayNumberClass' : 'cvDayNumber',
-			'currentDateLabelClass' : 'cvCurrentDateLabel',
-			'innerClass' : 'cvInner',
-			'dateFieldClass' : 'cvDate',
-			'previewTextContainerClass' : 'cvPreviewText',
-			'fullTextContainerClass' : 'cvFullText',
-			
-			'currentDateLabelText' : 'today'
-		},
-		
-		/**
-		 * Initialise the given element as a calendar view.
+		 * Get the data from the given set of &lt;li&gt; elements.
 		 * 
-		 * @param elem &lt;ul&gt; element to convert into a calendar view
-		 * @param options See $.fn.calendarView(options)
+		 * @param liElems Elements to inspect.
+		 * 
+		 * @return Key-value map of data from the list.
 		 */
-		_init : function(elem, options) {
-			function getData(liElems) {
-				data = [];
-				for (var i=0; i<liElems.length; i++) {
-					var liElem = liElems[i];
-					var dateElem = $(liElem).find('.'+options.dateFieldInputClass);
+		getData : function(liElems) {
+			data = [];
+			for (var i=0; i<liElems.length; i++) {
+				var liElem = liElems[i];
+				if (liElem.nodeName.toLowerCase() == 'li') {
+					liElem = $(liElem);
+					var dateElem = liElem.find('.'+this.options.dateFieldInputClass);
 					var date = Date.parse($(dateElem).text());
-					var previewTextElem = $(liElem).find('.'+options.previewTextInputClass);
-					var fullTextElem = $(liElem).find('.'+options.fullTextInputClass);
+					var previewTextElem = liElem.find('.'+this.options.previewTextInputClass);
+					var fullTextElem = liElem.find('.'+this.options.fullTextInputClass);
 					date.set({ 'hour' : 0, 'minute' : 0, 'second' : 0, 'millisecond' : 0 });
 					data[data.length] = {
 						'date' : date,
@@ -83,140 +106,206 @@
 						'fullText' : fullTextElem.html()
 					};
 				}
-				return data;
-			};
-			
-			function swapContainer(elem) {
-				var container = $('<div class="' + options.calendarViewClass + '"></div>');
-				elem = $(elem);
-				elem.parent().append(container);
-				return container;
-			};
-			
-			function createNestedDiv(container, outerClass) {
-				var outer = $('<div class="' + outerClass + '"></div>');
-				var inner = $('<div class="' + options.innerClass + '"></div>');
-				container.append(outer);
-				outer.append(inner);
-				return inner;
-			};
-			
-			function createMonthYearDiv(container, date) {
-				var div = createNestedDiv(container, options.monthYearClass);
-				div.append(date.toString('MMMM yyyy'));
-				return div;
-			};
-			
-			function createDayNameDiv(container) {
-				var div = $('<div class="' + options.dayNameDivClass + '"></div>');
-				container.append(div);
-				return div;
-			};
-			
-			function createDayDiv(container, date, isOutOfMonth, dateDiff) {
-				var cssClass = options.dayClass + ' ' + (
-					isOutOfMonth ? 
-						options.dayOutOfMonthClass : 
-						dateDiff == 0 ? 
-							options.currentDateClass : 
-							dateDiff < 0 ? 
-								options.pastDateClass : 
-								options.futureDateClass);
-				var div = createNestedDiv(container, cssClass);
-				div.append('<div class="'+ options.dayNumberClass + '">' + date.toString('dd') + '</div>');
-				if (dateDiff == 0) {
-					div.append('<div class="' + options.currentDateLabelClass + '">' + options.currentDateLabelText + '</div>');
-				}
-				return div;
-			};
-			
-			function fixDate(date) {
-				if (!date) {
-					date = new Date();
-				} else if (typeof date == 'string') {
-					date = parseDate(date);
-				} else if (typeof date == 'number') {
-					date = new Date(date);
-				}
-				date.moveToFirstDayOfMonth();
-				date.set({ 'hour' : 0, 'minute' : 0, 'second' : 0, 'millisecond' : 0 });
-				return date;
-			};
-			
-			function getCurrentDate() {
-				var date = new Date();
-				date.set({ 'hour' : 0, 'minute' : 0, 'second' : 0, 'millisecond' : 0 });
-				return date;
-			};
-			
-			function processDate(date, data) {
-				if (date.getDay() == 0) {
-					container.append('<div style="clear:both;"></div>');
-				}
-				if (dayNameCount++ < 7) {
-					createNestedDiv(dayNameDiv, options.dayNameLabelClass)
-						.append('<strong>'+date.toString('ddd')+'</strong>')
-						.parent().width(size);
-				}
-				var dayDiv = createDayDiv(container, date, date.getMonth() != month, date.compareTo(now));
-				dayDiv.parent().width(size).height(size);
-				// TODO get the actual padding and border sizes, not just -10 here
-				dayDiv.width(size-10).height(size-10);
-				if (date.getMonth() == month) {
-					for (var i=0; i<data.length; i++) {
-						datum = data[i];
-						if (datum['date'].equals(date)) {
-							dayDiv.addClass('event');
-							(function(dayDiv, previewText, fullText) {
-								var previewTextContainer = $('<div class="' + options.previewTextContainerClass + '"><span>' + previewText + '</span></div>');
-								dayDiv.append(previewTextContainer);
-								var fullTextContainer = $('<div class="' + options.fullTextContainerClass + '"><span>' + fullText + '</span></div>');
-								fullTextContainer.css('display', 'none');
-								$(container).append(fullTextContainer);
-								previewTextContainer.css('cursor', 'pointer');
-								previewTextContainer.mouseenter(function(event) {
-									var offset = dayDiv.parent().offset();
-									fullTextContainer.css({
-										'position' : 'absolute',
-										'left' : offset['left'] + dayDiv.width(),
-										'top' : offset['top']
-									});
-									fullTextContainer.fadeIn();
-								});
-								previewTextContainer.mouseleave(function(event) {
-									fullTextContainer.fadeOut();
-								});
-							})($(dayDiv), datum['previewText'], datum['fullText']);
-						}
-					}
-				}
-			};
-			
-			var x = true;
-			
-			var data = getData($('li', elem));
-			var container = swapContainer($(elem));
-			// TODO Get the actual padding and border here, instead of just -4
-			var size = (container.width() - 4) / 7;
-			var date = fixDate(options.currentDate);
-			var now = getCurrentDate();
-			var month = date.getMonth();
-			var monthYearDiv = createMonthYearDiv(container, date);
-			var dayNameDiv = createDayNameDiv(container);
-			var dayNameCount = 0;
-
+			}
+			return data;
+		},
+		
+		/**
+		 * Helper method.
+		 * Create a nested div, that is, a div with another div inside it.  The
+		 * outer div has the given class, and the inner div has the innerClass
+		 * from the options object.  The outer div is appended to the given
+		 * parent.
+		 * 
+		 * @param param Parent to add the nested div to.
+		 * @param outerClass Class for the outer div.
+		 * 
+		 * @return Inner div.
+		 */
+		createNestedDiv : function(parent, outerClass) {
+			var outer = $('<div class="' + outerClass + '"></div>');
+			var inner = $('<div class="' + this.options.innerClass + '"></div>');
+			parent.append(outer);
+			outer.append(inner);
+			return inner;
+		},
+		
+		/**
+		 * Create a div containing the month name and year, and month
+		 * navigation links.
+		 * 
+		 * @param date The date to create the calendar view for.
+		 * 
+		 * @return Created div.
+		 */
+		createMonthYearDiv : function(date) {
+			var div = this.createNestedDiv(this.container, this.options.monthYearClass);
+			var previousMonthLink = $('<a href="#" class="' + this.options.previousMonthLinkClass + '">' + this.options.previousMonthLinkText + '</a>');
+			var nextMonthLink = $('<a href="#" class="' + this.options.nextMonthLinkClass + '">' + this.options.nextMonthLinkText + '</a>');
+			previousMonthLink.jcalendarview = this;
+			nextMonthLink.jcalendarview = this;
+			previousMonthLink.click(function(event) {
+				this.jcalendarview.date.addMonth(-1);
+				this.jcalendarview.render();
+			});
+			nextMonthLink.click(function(event) {
+				this.jcalendarview.date.addMonth(1);
+				this.jcalendarview.render();
+			});
+			div.append(previousMonthLink);
+			div.append(nextMonthLink);
+			div.append(date.toString('MMMM yyyy'));
+			return div;
+		},
+		
+		/**
+		 * Create a div containing the names of the days.
+		 * 
+		 * @param size Size of each day name div.
+		 * 
+		 * @return Created div.
+		 */
+		createDayNameDiv : function(size) {
+			var div = $('<div class="' + this.options.dayNameDivClass + '"></div>');
+			var date = new Date();
 			date.moveToDayOfWeek(0, -1);
-			container.css('display', 'inline-block');
-			
-			while (date.getMonth() <= month || date.getDay() > 0) {
-				processDate(date, data);
+			for (var i=0; i<7; i++) {
+				this.createNestedDiv(div, this.options.dayNameLabelClass)
+					.append('<strong>'+date.toString('ddd')+'</strong>')
+					.parent().width(size);
 				date.addDays(1);
 			}
+			this.container.append(div);
+			return div;
+		},
+		
+		/**
+		 * Create a div for a single day, not including the data.
+		 * 
+		 * @param date Date of the day.
+		 * @param isOutOfMonth True if the day is out of the current month.
+		 * @param dateDiff Number of days between the current date and the
+		 *   given date (positive for future dates, negative for past dates).
+		 *   
+		 * @return Created div.
+		 */
+		createDayDiv : function(date, isOutOfMonth, dateDiff) {
+			var cssClass = this.options.dayClass + ' ' + (
+				isOutOfMonth ? 
+					this.options.dayOutOfMonthClass : 
+					dateDiff == 0 ? 
+						this.options.currentDateClass : 
+						dateDiff < 0 ? 
+							this.options.pastDateClass : 
+							this.options.futureDateClass);
+			var div = this.createNestedDiv(this.container, cssClass);
+			div.append('<div class="'+ this.options.dayNumberClass + '">' + date.toString('dd') + '</div>');
+			if (dateDiff == 0) {
+				div.append('<div class="' + this.options.currentDateLabelClass + '">' + this.options.currentDateLabelText + '</div>');
+			}
+			return div;
+		},
+		
+		/**
+		 * Fix the given date by ensuring it is a Date object and represents a
+		 * date at midnight.
+		 * 
+		 * @param date Date to fix.
+		 * 
+		 * @return Fixed date.
+		 */
+		fixDate : function(date) {
+			if (!date) {
+				date = new Date();
+			} else if (typeof date == 'string') {
+				date = parseDate(date);
+			} else if (typeof date == 'number') {
+				date = new Date(date);
+			}
+			date.set({ 'hour' : 0, 'minute' : 0, 'second' : 0, 'millisecond' : 0 });
+			return date;
+		},
+		
+		/**
+		 * Draw the calendar view at the current date.
+		 */
+		render : function() {
+			// Initialise
+			var date = new Date(this.date.getTime());
+			date.moveToFirstDayOfMonth();
 			
-			container.append('<div style="clear:both;"></div>');
-			$(elem).css('display', 'none');
-			return container;
+			var data = this.getData($('li', this.elem));
+			var size = Math.floor(this.container.innerWidth() / 7.0);
+			var month = date.getMonth();
+			var monthYearDiv = this.createMonthYearDiv(date);
+			var dayNameDiv = this.createDayNameDiv(size);
+			
+			// Process each date in order
+			date.moveToDayOfWeek(0, -1);
+			while (date.getMonth() <= month || date.getDay() > 0) {
+				this.renderDate(date, month, data, size);
+				date.addDays(1);
+			}
+		},
+		
+		/**
+		 * Draw a single date.
+		 * 
+		 * @param date Date to draw.
+		 * @param month Month being drawn.
+		 * @param data Data for the calendar.
+		 * @param size Size of the boxes in px.
+		 */
+		renderDate : function(date, month, data, size) {
+			var now = this.fixDate(new Date());
+			if (date.getDay() == 0) {
+				this.container.append('<div style="clear:both;"></div>');
+			}
+			var dayDiv = this.createDayDiv(date, date.getMonth() != month, date.compareTo(now));
+			dayDiv.parent().width(size).height(size);
+			// TODO get the actual padding and border sizes, not just -10 here
+			dayDiv.width(size-10).height(size-10);
+			if (date.getMonth() == month) {
+				for (var i=0; i<data.length; i++) {
+					datum = data[i];
+					if (datum['date'].equals(date)) {
+						dayDiv.addClass('event');
+						this.processDayData($(dayDiv), datum['previewText'], datum['fullText']);
+					}
+				}
+			}
+		},
+		
+		/**
+		 * Process a single piece of data on a given date.
+		 * 
+		 * @param dayDiv Div representing the relevant date.
+		 * @param previewText Text to display directly on the calendar.
+		 * @param fullText Text to display in the popup div.
+		 */
+		processDayData : function(dayDiv, previewText, fullText) {
+			var previewTextContainer = $('<div class="' + this.options.previewTextContainerClass + '"><span>' + previewText + '</span></div>');
+			dayDiv.append(previewTextContainer);
+			var fullTextContainer = $('<div class="' + this.options.fullTextContainerClass + '"><span>' + fullText + '</span></div>');
+			fullTextContainer.css('display', 'none');
+			this.container.append(fullTextContainer);
+			previewTextContainer.css('cursor', 'pointer');
+			previewTextContainer.mouseenter(function(event) {
+				var offset = dayDiv.parent().offset();
+				var left = offset['left'] + dayDiv.parent().width();
+				if (left + fullTextContainer.outerWidth() > $(document.body).width()) {
+					left = offset['left'] - fullTextContainer.outerWidth(true);
+				}
+				fullTextContainer.css({
+					'position' : 'absolute',
+					'left' : left,
+					'top' : offset['top']
+				});
+				fullTextContainer.fadeIn();
+			});
+			previewTextContainer.mouseleave(function(event) {
+				fullTextContainer.fadeOut();
+			});
 		}
-	};
-	
+	});
 })(jQuery);
